c22aa891b9a366107473f503ac60919ec4cfe875
Roger Gammans <rgammans@gammascience.co.uk>
2025-07-19 13:51:02 +0100

    Close stdout/stderr once kubectl has completed
    this means the completions scripts don't hang around waiting
    for kubeconfig to be destroyed. (oops)

--- src/init-env.sh
+++ src/init-env.sh
@@ -4,6 +4,12 @@ k8senv_cmd() {
  (
      . $K8SENV_LIB_DIR/runcmd.sh;
        "$@";
+      # Close stdout/stdin as the are held open
+      # by the cleanup; but shell subst and eval need
+      # to know the output is complete. (which is need)
+      # to make completions work amogst other things
+      exec 2>&-
+      exec 1>&-
   )
 
 }
