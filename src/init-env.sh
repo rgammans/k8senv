ed567ec2619efd8b2546e0f9a1c510183b3aa74d
Roger Gammans <rgammans@gammascience.co.uk>
2025-07-30 17:46:59 +0100

    Improve file descriptor handling.
    Rather than trying to close the stdout/stderr after launching the
    trap, don't bind them to the trap script in the first place
    
    Also improve chnace of correct cleanup by using nohup; to ensuring
    the cleanup script is protected from some early termination cases.

--- src/init-env.sh
+++ src/init-env.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-k8senv_cmd() {
+k8s_cmd() {
  (
      . $K8SENV_LIB_DIR/runcmd.sh;
        "$@";
-      # Close stdout/stdin as the are held open
-      # by the cleanup; but shell subst and eval need
-      # to know the output is complete. (which is need)
-      # to make completions work amogst other things
-      exec 2>&-
-      exec 1>&-
   )
 
 }
--- src/runcmd.sh
+++ src/runcmd.sh
@@ -8,7 +8,7 @@ mkdir -p $(dirname $KUBECONFIG)
 export KLOCKPID=$BASHPID
 mkdir -p /run/user/$UID/k8senv/locks/$K8SENV_CLUSTER
 touch /run/user/$UID/k8senv/locks/$K8SENV_CLUSTER/$KLOCKPID
-trap '($K8SENV_LIB_DIR/cleanup.sh)&' 0
+trap '(nohup $K8SENV_LIB_DIR/cleanup.sh < /dev/null >/dev/null 2>/dev/null)&' 0
 # Now trap is setup we can cretae the file; so there
 # do race-hardazrd signal path,.
 if [ ! -s $KUBECONFIG ]; then
