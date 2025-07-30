bb2b77feec9a98575f21445ed78fd07178a237ab
Roger Gammans <rgammans@gammascience.co.uk>
2025-07-30 18:22:26 +0100

    Improve file permissions on kubeconfig
    Use umask to ensure created kubeconfig file, and dir is only owner
    read/writable.
    
    (It should be stored in /var/run/UID which is normally 0700 anyway
     but it doesn't hurt to be careful )

--- src/runcmd.sh
+++ src/runcmd.sh
@@ -4,7 +4,7 @@
 # traps it for removal
 
 KUBECONFIG=/run/user/$UID/k8senv/cfg/$K8SENV_CLUSTER
-mkdir -p $(dirname $KUBECONFIG)
+(umask 077; mkdir -p $(dirname $KUBECONFIG) )
 export KLOCKPID=$BASHPID
 mkdir -p /run/user/$UID/k8senv/locks/$K8SENV_CLUSTER
 touch /run/user/$UID/k8senv/locks/$K8SENV_CLUSTER/$KLOCKPID
@@ -12,6 +12,6 @@ trap '(nohup $K8SENV_LIB_DIR/cleanup.sh < /dev/null >/dev/null 2>/dev/null)&' 0
 # Now trap is setup we can cretae the file; so there
 # do race-hardazrd signal path,.
 if [ ! -s $KUBECONFIG ]; then
-    $GETKUBE $K8SENV_CLUSTER > $KUBECONFIG ;
+    (umask 077; $GETKUBE $K8SENV_CLUSTER > $KUBECONFIG ) ;
 fi
 export KUBECONFIG
